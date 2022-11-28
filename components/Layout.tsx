5ca4ee3f72aef3007272711977a733214b5b6063
Jean-Claude Pratt-Delzenne <prattjames4@gmail.com>
2022-11-28 20:30:38 +0100

    layout fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children, className = '' }: LayoutType) => {
     const router = useRouter()
 
     return (<>
-        <div className={`relative z-10 flex flex-col items-center justify-center w-full h-full min-h-screen pb-14`}>
+        <div className={`relative z-10 flex flex-col items-center justify-center w-full h-full pb-14`}>
             <div className={`luminous relative w-full h-52 flex justify-between mb-20 md:mb-5 px-6 md:px-16`}>
                 <img 
                     className="cursor-pointer" 
@@ -23,7 +23,7 @@ const Layout = ({ children, className = '' }: LayoutType) => {
                     onClick={() => router.push('/')}
                 />
             </div>
-            <div className={`w-full h-full min-h-screen flex flex-col items-center ${className}`}>
+            <div className={`w-full h-full flex flex-col items-center ${className}`}>
                 {children}
             </div>
         </div>
